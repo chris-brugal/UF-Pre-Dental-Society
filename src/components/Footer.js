03bd758502767f910e4909d4c988b70081a41046
Diana Feng <dianafeng@ufl.edu>
2021-03-06 23:12:53 -0500

    start of frontend in react

---
+++ src/components/Footer.js
@@ -0,0 +1,44 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+import { Button } from './Button';
+import './Footer.css';
+
+function Footer() {
+    return (
+        <div className='footer-container'>
+            <section className="footer-subscription">
+                <p className="footer-subscription-heading">Sign up to be added to our Listserv!</p>
+                <div className="input-areas">
+                    <form>
+                        <input type="email" name="email" placeholder="Your Email"
+                        className="footer-input"/>
+                        <Button buttonStyle='btn--outline'>Submit</Button>
+                    </form>
+                </div>
+                <p className="footer-subscription-text">Contact our president at pds.president@gmail.com</p>
+            </section>
+            <section className="social-media">
+                <div className="social-media-wrap">
+                    <div className="social-icons">
+                        <Link className="social-icon-link facebook"
+                        to={{ pathname: "https://www.facebook.com/UFPDS/" }}
+                        target='_blank'
+                        aria-label='Facebook'
+                        >
+                            <i className="fab fa-facebook-f"></i>
+                        </Link>
+                        <Link className="social-icon-link instagram"
+                        to={{ pathname: "https://www.instagram.com/predentalsociety/"}}
+                        target='_blank'
+                        aria-label='Instagram'
+                        >
+                            <i className="fab fa-instagram"></i>
+                        </Link>
+                    </div>
+                </div>
+            </section>
+        </div>
+    )
+}
+
+export default Footer
---
+++ src/components/HeroSection.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import './HeroSection.css';
+import '../App.css';
+
+function HeroSection() {
+    return (
+        <div className ='hero-container'>
+            <section className="welcome">
+            <h1>WELCOME!</h1>
+            <p>The University of Florida Pre-Dental Society welcomes all UF students interested in pursuing Dentistry. Our organization provides students with a community of driven, creative, and diverse individuals striving to make giant leaps in the Health Professions. We provide members with opportunities for service events, as our motto goes: we provide "Service with a Smile!" Pre-Dental students part of our organization also participate in social and fundraising events that bring everyone together, support our local community, and improve manual dexterity.</p>
+            </section>
+        </div>
+    )
+}
+
+export default HeroSection;
