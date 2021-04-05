d203a9984e7c7ad430565f7c05ca4532d04746aa
bmcadam99 <44203887+bmcadam99@users.noreply.github.com>
2021-04-05 14:39:40 -0400

    Test updated

--- client/test/test.js
+++ client/test/test.js
@@ -2,17 +2,17 @@ var assert = require('assert');
 describe('Array', function() {
   describe('#indexOf()', function() {
     it('should return -1 when the value is not present', function() {
-      assert.equal([1, 2, 3].indexOf(4), -1);
+      assert.strictEqual([1, 2, 3].indexOf(4), -1);
     });
   });
   describe('#sort()', function() {
     it('should return true when the values are in order', function() {
-      assert.deepEqual([5,3,6,1].sort(), [1,3,5,6]);
+      assert.deepStrictEqual([5,3,6,1].sort(), [1,3,5,6]);
     });
   });
   describe('#reverse()', function() {
     it('should return the reverse of the array', function() {
-      assert.deepEqual([1,2,3,4,5].reverse(), [5,4,3,2,1]);
+      assert.deepStrictEqual([1,2,3,4,5].reverse(), [5,4,3,2,1]);
     });
   });
 });
