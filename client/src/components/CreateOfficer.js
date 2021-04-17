24d5adb5e2a44e2219ee886d06626d2da299ac7e
Chris <chrisbrugal@yahoo.com>
2021-04-17 14:21:20 -0400

    Updated officers page, not connected to back yet

---
+++ client/src/components/CreateOfficer.js
@@ -0,0 +1,86 @@
+import React, { Component } from 'react';
+import { Container, ListGroup, ListGroupItem, Button } from 'reactstrap'; //install reactstrap
+import { CSSTransition, TransitionGroup } from 'react-transition-group';
+import './CreateOfficer.css';
+import { connect } from 'react-redux';
+import { getOfficer, deleteOfficer, editOfficer, addOfficer } from '../actions/officerActions';
+import PropTypes from 'prop-types';
+import photo from '../images/bowling.jpg';
+
+class CreateOfficer extends Component {
+
+    /*
+    state = {
+        officers: [
+            { id: uuid(), name: 'Nate'},
+            { id: uuid(), name: 'Diana'},
+            { id: uuid(), name: 'Bobby'}
+        ]
+    }
+ */
+
+compoentDidMount() {
+this.props.getOfficer();
+}
+
+onDeleteClick = (id) => {
+    this.props.deleteOfficer(id);
+}
+
+//OfficerPic class here
+render() {
+const { officers } = this.props.officer;
+//const { officers } = this.state;
+return (
+    
+    <Container>
+         <h1>test</h1>
+        
+        <ListGroup>
+            <TransitionGroup className="officer-pics">
+                {officers.map(({ id, displayName, position, bio }) => (
+                <CSSTransition key={id} timeout={500} classNames="fade">
+                <ListGroupItem>
+                    <Button
+                    className="remove-btn"
+                    color="danger"
+                    size="sm"
+                    onClick={this.onDeleteClick.bind(this, id)}
+                    >
+                    &times;
+                    </Button>
+                   
+        <div class="hoverPhoto">
+            <img src={photo} id='photo' width="400" height="400" alt="tree" />
+            <div class="content" >
+                <h1 className='name'>{displayName}</h1>
+                    <h3 className='title'>{position}</h3>
+                    
+                    <h3 className='bio' >{bio}</h3>
+             </div>
+        </div>
+       
+    
+
+                   
+                </ListGroupItem>
+                </CSSTransition>
+                ))}
+                </TransitionGroup >
+        </ListGroup>
+    </Container>
+   
+            );
+        }
+    }
+
+CreateOfficer.propTypes = {
+    getOfficer: PropTypes.func.isRequired,
+    officer: PropTypes.object.isRequired
+}
+
+const mapStateToProps = (state) => ({
+officer: state.officer
+});
+
+export default connect(mapStateToProps, {getOfficer, addOfficer, editOfficer, deleteOfficer })(CreateOfficer);
--- client/src/components/pages/OurOfficers.js
+++ client/src/components/pages/OurOfficers.js
@@ -1,12 +1,16 @@
 import React from 'react';
 import '../../App.css';
 import Footer from '../Footer';
+import CreateOfficer from '../CreateOfficer';
+import OfficerModal from '../OfficerModal';
 
 export default function OurOfficers () {
     return (
         <>
         <br></br>
         <h1 className='our-officers'>OUR OFFICERS</h1>
+        <CreateOfficer />
+        <OfficerModal />
         <Footer />
         </>
         )
