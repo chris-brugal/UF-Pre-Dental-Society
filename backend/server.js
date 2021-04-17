7cf74bcc68f1768622909153ff8c6b83de505316
Chris <chrisbrugal@yahoo.com>
2021-04-17 00:04:53 -0400

    fixed server.js for dep

--- backend/server.js
+++ backend/server.js
@@ -38,5 +38,12 @@ app.use('/api/events', events);
 app.use('/api/officers', officers)
 app.use('/api/users', users)
 
+if (process.env.NODE_ENV === 'production') {
+    app.use(express.static('client/build'));
+    app.get('*', (req, res) => {
+      res.sendFile(path.resolve(__dirname, '..','client', 'build', 'index.html'));
+    });
+  }
+
 app.listen(PORT, () =>  
     console.log("Server is running on Port: " + PORT) );
