9aae5d56d4b97ce1788d9f03cf11d35e22a8b958
Diana Feng <dianafeng@ufl.edu>
2021-04-18 16:12:57 -0400

    footer formatting updated

--- client/src/components/Footer.js
+++ client/src/components/Footer.js
@@ -7,15 +7,9 @@ function Footer() {
     return (
         <div className='footer-container'>
             <section className="footer-subscription">
-                <p className="footer-subscription-heading">Sign up to be added to our Listserv!</p>
-                <div className="input-areas">
-                    <form>
-                        <input type="email" name="email" placeholder="Your Email"
-                        className="footer-input"/>
-                        <Button class="btn btn-light">Submit</Button>
-                    </form>
-                </div>
-                <p className="footer-subscription-text-1">Contact our president at pds.president@gmail.com</p>
+                <p className="footer-subscription-heading">Contact our president at pds.president@gmail.com</p>
+                <a target="_blank" className="btn btn-secondary" href="mailto:pds.president@gmail.com">SIGN UP FOR OUR LISTSERV!</a>
+                <p className="footer-subscription-text-1">Put "Listserv Request" in the Subject</p>
                 <p className="footer-subscription-text-2">Text @ufpds2020 to 81010 to stay updated!</p>
             </section>
             <section className="social-media">
